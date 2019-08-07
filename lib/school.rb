b8daa26bc2a6f17310b7264e00bb96d40d0de0a1
github username <github email address>
2019-08-07 07:29:23 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+    attr_accessor :roster
+    def initialize(name)
+        @roster = {}
+        @name = name
+    end
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        sorted = {}
+        @roster.each {|k,v| sorted[k] = v.sort }
+        sorted
+    end
+end
+school = School.new("Bayside High School")
